aebb20edb9e3ac1c203e733f4f67b0936d6c92fe
Kingwebbie001 <71094890+Kingwebbie001@users.noreply.github.com>
2025-09-20 23:32:39 +1000

    Version 1.1
    - Removed background images in favour of a static image attached to the body.
    - Refactored style sheets so that main.css is universal, with page specific style sheets for each page.
    - Added pages: about and affiliates.
    - Fixed scaling of elements that comprise the page layout.
    - Implemented style variables in main.css to make iteration easier.
    - Added page footer with a link to the privacy policy, contact, and a website feedback form.
    - Moved the principles section to underneath the video in index.html.
    - Changed website colour scheme to match logo.
    - Refactored JavaScript scripts to be page dependent with a main script for all pages.

--- i-CHAt/scripts/index.js
+++ i-CHAt/scripts/index.js
@@ -1,27 +1,16 @@
-// Get the elements from the DOM
-const hamburgerButton = document.getElementById('hamburger-menu');
-const directoryMenu = document.getElementById('directory-container');
 
-// Function to toggle the menu visibility
-function toggleMenu() {
-    directoryMenu.classList.toggle('show');
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const acronymHeader = document.getElementById('acronymHeader');
 
-// Add a click event listener to the hamburger button to toggle the menu
-hamburgerButton.addEventListener('click', function(event) {
-    // Stop the click event from propagating to the document
-    event.stopPropagation();
-    toggleMenu();
-});
-
-// Add a click event listener to the entire document
-document.addEventListener('click', function(event) {
-    const isClickInsideMenu = directoryMenu.contains(event.target);
-    const isClickOnHamburger = hamburgerButton.contains(event.target);
-
-    // If the menu is open AND the click is NOT inside the menu AND the click is NOT on the hamburger button
-    if (directoryMenu.classList.contains('show') && !isClickInsideMenu && !isClickOnHamburger) {
-        // Close the menu
-        toggleMenu();
+  function updateHeader() {
+    if (window.innerWidth <= 600) {
+      acronymHeader.innerHTML = 'Intergenerational Community Homeshare Activity'; 
+    } else {
+      acronymHeader.innerHTML = 'Intergenerational Community Homeshare Activity (I-CHAt)';
     }
+  }
+
+  updateHeader();
+
+  window.addEventListener('resize', updateHeader);
 });
